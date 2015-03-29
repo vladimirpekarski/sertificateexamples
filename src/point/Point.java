185ac8175fb5e038ace8d5f42438d369cd956ac7
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-03-29 19:11:55 +0300

    incorrect implementation equals 1st

---
+++ src/point/Point.java
@@ -0,0 +1,31 @@
+package point;
+
+/**
+ * Created by asus on 29.03.15.
+ */
+public class Point {
+    private int xPos;
+    private int yPos;
+
+    public Point(int xPos, int yPos) {
+        this.xPos = xPos;
+        this.yPos = yPos;
+    }
+
+    public boolean equals(Point other) {
+        if (other == null) {
+            return false;
+        }
+
+        return ((xPos == other.xPos) && (yPos == other.yPos));
+    }
+
+    public static void main(String[] args) {
+        Point point1 = new Point(10, 20);
+        Point point2 = new Point(50, 100);
+        Point point3 = new Point(10, 20);
+
+        System.out.println("point1 equals point2 is " + point1.equals(point2));
+        System.out.println("point1 equals point3 is " + point1.equals(point3));
+    }
+}

4ee484da603c18f9d1bb68fdd0e85648514104b9
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-04-01 21:26:20 +0300

    final class example

--- src/appcanvas/Canvas.java
+++ src/appcanvas/Canvas.java
@@ -4,7 +4,7 @@ import graphicshape.Circle;
 /**
  * Created by v.pekarsky on 26.03.15.
  */
-public class Canvas {
+final public class Canvas {
     void gerArea() {
         Circle circle = new Circle();
         circle.area(); //public

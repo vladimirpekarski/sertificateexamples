e0612b0f5250034c729307e812b2022d0e9c2e5f
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-04-04 13:05:09 +0300

    inner classes

---
+++ src/innerclasses/innerclasses/Outer.java
@@ -0,0 +1,5 @@
+package innerclasses.innerclasses;
+
+public class Outer { // an outer class has an inner class
+    class Inner{}
+}
---
+++ src/innerclasses/innerclasses/OuterInt.java
@@ -0,0 +1,5 @@
+package innerclasses.innerclasses;
+
+public class OuterInt { // an outer class has an inner interface
+    interface Inner{}
+}

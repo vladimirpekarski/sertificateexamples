d925f1ed7b4d9951c2bf2f789a88d14e9ccdb0ee
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-04-17 17:54:04 +0300

    generics - raw type Vector

---
+++ src/generics/OldContainerTest.java
@@ -0,0 +1,15 @@
+package generics;
+
+import java.util.Vector;
+
+public class OldContainerTest {
+    public static void main(String[] args) {
+        Vector vector = new Vector();
+        vector.add(10.0f);
+        vector.add(100.0);
+        for (int i = 0; i < vector.size(); i++) {
+//            Float temp = (Float) vector.get(i);
+            System.out.println(vector.get(i));
+        }
+    }
+}

ba5386e7109ddbecc32c77fd31cce8886dc67646
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-04-17 14:09:59 +0300

    factory pattern calendar

---
+++ src/pattern/factorydesignpattern/TestCalendar.java
@@ -0,0 +1,10 @@
+package pattern.factorydesignpattern;
+
+import java.util.Calendar;
+
+public class TestCalendar {
+    public static void main(String[] args) {
+        Calendar calendar = Calendar.getInstance();
+        System.out.println(calendar);
+    }
+}

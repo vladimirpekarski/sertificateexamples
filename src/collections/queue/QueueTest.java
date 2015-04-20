fe00306eef5c3e20e967f1d62155a991cc9f6aa9
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-04-20 17:25:23 +0300

    queue

---
+++ src/collections/queue/QueueTest.java
@@ -0,0 +1,21 @@
+package collections.queue;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueTest {
+    public static void main(String[] args) {
+        Queue<String> loginSequence = new LinkedList<>();
+
+        loginSequence.add("Harrison");
+        loginSequence.add("McCartney");
+        loginSequence.add("Starr");
+        loginSequence.add("Lennon");
+
+        System.out.println("The login sequence is: " + loginSequence);
+
+        while (!loginSequence.isEmpty()) {
+            System.out.println("Removing " + loginSequence.remove());
+        }
+    }
+}

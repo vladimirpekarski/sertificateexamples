3744d9842a48861f59996a74bf69faef7ffdc770
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-04-23 20:52:53 +0300

    strings simple 2 examples

---
+++ src/string/SearchString1.java
@@ -0,0 +1,8 @@
+package string;
+
+public class SearchString1 {
+    public static void main(String[] args) {
+        String str = "OCPJP";
+        System.out.println("Character 3 occurs at index: " + str.indexOf('J'));
+    }
+}
---
+++ src/string/SearchString2.java
@@ -0,0 +1,8 @@
+package string;
+
+public class SearchString2 {
+    public static void main(String[] args) {
+        String str = "I am preparing for OCPJP";
+        System.out.println("Substring \"for\" occurs at index " + str.indexOf("for"));
+    }
+}

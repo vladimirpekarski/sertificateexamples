85d495c83ce5883b4fa2fb353fdc795bf2633f47
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-04-28 19:15:35 +0300

    Compare two Path

---
+++ src/nio/PathCompare1.java
@@ -0,0 +1,17 @@
+package nio;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathCompare1 {
+    public static void main(String[] args) {
+        Path path1 = Paths.get("Test");
+        Path path2 = Paths.get("D:\\books\\JAVA\\Sertificate\\sertification\\sertification\\Test");
+
+        System.out.println("(path1.compareTo(path2) == 0: " + (path1.compareTo(path2) == 0));
+
+        System.out.println("path1.equals(path2) is: " + path1.equals(path2));
+
+        System.out.println("path2.equals(path1.toAbsolutePath) is " + path2.equals(path1.toAbsolutePath()));
+    }
+}

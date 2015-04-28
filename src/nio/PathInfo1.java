244c1bcd26799ae952780f1c21cc5470ff5379ed
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-04-28 19:08:07 +0300

    Path

---
+++ src/nio/PathInfo1.java
@@ -0,0 +1,23 @@
+package nio;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+/**
+ * Created by asus on 28.04.15.
+ */
+public class PathInfo1 {
+    public static void main(String[] args) {
+        Path testFilePath = Paths.get("D:\\books\\JAVA\\firstFile2.txt");
+
+        System.out.println("Printing file information: ");
+        System.out.println("\t file name: " + testFilePath.getFileName());
+        System.out.println("\t root of the path: " + testFilePath.getRoot());
+        System.out.println("\t parent of the target: " + testFilePath.getParent());
+
+        System.out.println("Print elements of pathL ");
+        for (Path path : testFilePath) {
+            System.out.println("\t path elements: " + path);
+        }
+    }
+}
---
+++ src/nio/PathInfo2.java
@@ -0,0 +1,24 @@
+package nio;
+
+import java.io.IOException;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+public class PathInfo2 {
+    public static void main(String[] args) throws IOException {
+        Path testFilePath = Paths.get(".\\Test");
+        System.out.println("The file name is: " + testFilePath.getFileName());
+        System.out.println("It's URI is: " + testFilePath.toUri());
+        System.out.println("It's absolute path is: " + testFilePath.toAbsolutePath());
+        System.out.println("It's normalized path is: " + testFilePath.normalize());
+
+        Path testPathNormalized = Paths.get(testFilePath.normalize().toString());
+        System.out.println("It's normalized absolute path is: " +  testPathNormalized.toAbsolutePath());
+
+//        System.out.println("It's normalized real path is: " + testFilePath.toRealPath(LinkOption.NOFOLLOW_LINKS));
+
+        Path dirName = Paths.get("D:\\books\\JAVA\\Sertificate\\sertification\\sertification\\");
+        Path resolvedPath = dirName.resolve("Test");
+        System.out.println(resolvedPath);
+    }
+}

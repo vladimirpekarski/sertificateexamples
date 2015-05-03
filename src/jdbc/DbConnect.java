59854b277f0ac4ae81beb2f755ec36533e1cc37d
vladimirpekarski <vladimir.pekarski@gmail.com>
2015-05-03 21:22:46 +0300

    db connect (needs j connector)

---
+++ src/jdbc/DbConnect.java
@@ -0,0 +1,20 @@
+package jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class DbConnect {
+    public static void main(String[] args) {
+        String url = "jdbc:mysql://localhost:3306/";
+        String database = "addressBook";
+        String userName = "root";
+        String password = "Pa$$41372098";
+
+        try (Connection connection = DriverManager.getConnection(url + database, userName, password)){
+            System.out.println("Database connection: Successful");
+        } catch (Exception e) {
+            System.out.println("Database connection: Failed");
+            e.printStackTrace();
+        }
+    }
+}
